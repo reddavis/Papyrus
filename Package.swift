8b832c143e23b6c51e687bc7c206757b5464ff0c
Red Davis <me@red.to>
2021-03-16 06:03:54 +0000

    [Internal] set iOS requirement to 13

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 let package = Package(
     name: "Papyrus",
     platforms: [
-        .iOS("14.0"),
+        .iOS("13.0"),
         .macOS("11.0")
     ],
     products: [
