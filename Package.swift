38b862382857b0cfbf5a118b367f755d720f2e6b
Corey <coreyearleon@icloud.com>
2022-02-05 12:03:44 -0500

    move OS's back up

--- Package.swift
+++ Package.swift
@@ -7,10 +7,10 @@ import PackageDescription
 let package = Package(
     name: "Papyrus",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .iOS(.v15,
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(
